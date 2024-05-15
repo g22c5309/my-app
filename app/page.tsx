8321d132a9e1e76ae02745e93c746440564db446
Daleroy Chinomona <G22C5309@ict.ru.ac.za>
2024-05-15 13:34:40 +0200

    Changed from 'Hello Universe' to 'Hello Galaxy'

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
-        <h1>Hello Universe</h1> {/* Changed from 'Hello World' to 'Hello Universe' */}
+        <h1>Hello Galaxy</h1> {/* Changed from 'Hello Universe' to 'Hello Galaxy' */}
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -112,4 +112,5 @@ export default function Home() {
     </main>
   );
 }
+
 
